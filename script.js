4b2d4dcd96a747b0d21f353481a30780c775cb25
raniasar <raniasar307@gmail.com>
2025-12-03 13:56:07 +0100

    Add mobile menu toggle functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".burger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+burger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+});
